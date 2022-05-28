68d20e959b17e6521ab36695bee28b8b3ab6fd85
TheChiran <tonmoychiran449@gmail.com>
2022-05-28 22:31:36 +0600

    [strativ-virtual-classroom][Task-> 1. Tried to resolve password undefined issue

--- Authentication/auth.controller.js
+++ Authentication/auth.controller.js
@@ -32,7 +32,7 @@ const register = async(req,res)=>{
     const data = {
         username,
         email,
-        password: hashPassword,
+        final_password: final_password,
         role
     }
     const response = await generateUser(data);
